67d60e54e8f72c0ee56aa910c18838d0717c8373
NCStorey <ncsansom@hotmail.co.uk>
2023-02-24 21:17:32 +0000

    addition of question logic and fs

--- index.js
+++ index.js
@@ -1,21 +1,65 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+import inquirer from 'inquirer';
+import fs from 'fs';
 
-// array of questions for user
-const questions = [
+const generateMarkDown = (UserInput) =>
 
-];
+JSON.stringify(UserInput)
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+//array of question object that the user will be asked
+//question layout:
+//type - determines the type of input you are trying to gather
+//message - this is the questions that the user will be asked
+//name - this is the tag given to the question to allow you to grab the user input later
+//validate - error catching - if the user enters something you are not expecting 
 
-// function to initialize program
-function init() {
+const questions = [   
 
-}
+    {type: 'input',
+    message:'Please enter your project title',
+    name:'project_title',
+    validate: (input) => {
+        if (input == ''){
+        return 'Oops looks like you didnt enter a title. Please enter your project title'
+        }
+        else {return true}}
+    },
 
-// function call to initialize program
-init();
+    
+    {type: 'input',
+    message:'Can you tell us what your project is about?',
+    name:'project_description',
+    validate: (input) => {
+        if (input == ''){
+        return 'Oops looks like you didnt enter a anything. Please enter a description'
+        }
+        else {return true}}
+    },
+
+]
+
+//console log for user experiance
+console.log('------Lets make a README together!------')
+
+//prompt used to generate question in the CLI
+inquirer
+.prompt(questions)
+.then(answers => {
+    console.log(answers);
+
+    const markDownTemplate = generateMarkDown(answers)
+
+    fs.writeFile(`${answers.project_title}.md`, markDownTemplate , (error) => {
+        return error
+        ? console.log(error)
+        : console.log('success')
+    })
+})
+
+.catch((error) => {
+    if (error.isTtyError) {
+        console.log('Could not be rendered in the current environment')
+    } else {
+        console.log('Something else went wrong')
+    }
+})
+
---
+++ p.md
@@ -0,0 +1,1 @@
+{"project_title":"p","project_description":"p"}
